c641d2c3a6d4008db8c2edfdab1f2e3f344377bb
Bethany Nagel <beth0851@gmail.com>
2014-03-27 14:44:12 -0500

    write dfs method

--- tree.rb
+++ tree.rb
@@ -14,20 +14,40 @@ class Node
 
   def dfs(value)
     # if the given value is the same as this node - return self
+    if @value == value
+      return self
     # Otherwise call dfs on each child and see if they found it
-    # if the child found it, return the result
-    # otherwise move on to the next child
-    # If no child can find it, return nil
+    # if the child found it, return the result,  # otherwise move on to the next child
+    else
+      if @children.any?
+        @children.each do |child|
+          x = child.dfs(value)
+          return x if x
+        end
+    # If no child can find it, return nil  
+      end
+    return nil
+  end
+   
+    
   end
 
   def bfs(value, queue = [])
     # if the given value is the same as this node return self
+    if @value = value
+      return self
     # Otherwise add all of this nodes children to the queue
     # of nodes needed to be searched
-    #
-    # While the queue is not empty
-    # grab the first element in the queue (and remove it)
-    # run bfs on that node with the proper argument and get the result
+    else
+      queue << @children
+       # While the queue is not empty
+      while queue.length > 0
+        # grab the first element in the queue (and remove it)
+        # run bfs on that node with the proper argument and get the result
+        queue.shift.bfs(value, queue)
+      end
+    end
+    
     # if that node found what you're looking for, return the resulting node
     # Otherwise, move on to the next node
     #
